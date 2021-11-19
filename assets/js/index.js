a0bf89d1b82f79d2ea082d85fc1e970a5c314644
Daniel do Amaral Denardi <dan.denardi@gmail.com>
2021-11-19 17:40:53 -0300

    criado arquivo de javascript

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+//incluir transacoes de compra ou venda de mercadoria
+//Criar um extrato das transacoes incluidas na ordem que foram inseridas
+//Saldo final com destaque para lucro ou prejuizo
+//Persistir em Local Storage
+
+//Todos os campos devem ser preenchidos
+//Mascara no campo valor (padrao 10,90)
+//Ao inserir transacao salvar em local storage e atualizar a lista com extrato ja com o calculo atualizado
+//Acao limpar dados (onclick) deve apresentar mensagem de confirmacao, apagar informacoes e atualizar a lista
+
+//Duvida: limpar dados deve limpar toda a tabela ou apenas o(s) item(s) selecionados?
+
