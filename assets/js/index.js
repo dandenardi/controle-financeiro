4065aca2655af523d5b9e97cc253449d90166210
Daniel do Amaral Denardi <dan.denardi@gmail.com>
2021-11-22 17:19:10 -0300

    acrescentada persistencia de dados em local storage; acrescentadas estruturas de funcoes para exclusao e conversao de dados em JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,96 @@
+//funcao que fica responsavel por converter os dados obtidos de localStorage
+function handleStorage(){
+    var transactionsRaw = localStorage.getItem('transactions'); //pegando o item do localstorage de forma crua (string)
+    if (transactionsRaw != null){
+        var transactions = JSON.parse(transactionsRaw) //convertendo de string para o dado propriamente dito
+    } else {
+        var transactions = [];
+    }
+
+    return transactions;
+}
+    
 //incluir transacoes de compra ou venda de mercadoria
+function handleExchange(e){
+    //Ao inserir transacao salvar em local storage e 
+    e.preventDefault();
+    var transactions = handleStorage();
+
+     transactions.push({
+        name: e.target.elements['merch-name'].value,
+        value: e.target.elements['merch-value'].value,
+        kind: e.target.elements['transaction-type'].value,
+    })
+    
+    console.log(e.target.elements['merch-name'].value);
+    console.log(e.target.elements['merch-value'].value);
+    console.log(e.target.elements['transaction-type'].value);
+}
+
+
 //Criar um extrato das transacoes incluidas na ordem que foram inseridas
+function showTransactions() {
+
+    console.log("carregado showTransactions");
+
+   /*  for (var transaction in transactions){
+
+        document.querySelector('table tbody').innerHTML +=
+        `<tr>
+            <td>
+                ${transactions[transaction].name}
+            </td>
+            <td>
+                ${transactions[transaction].value}
+            </td>
+            <td>
+                ${transactions[transaction].kind}
+            </td>
+        </tr>`
+                   
+    }
+    `<tfoot>
+                <tr>
+                    <td>
+                        Total
+                    </td>
+                    <td>
+                        ${calculateBalance()}
+                    </td>
+                </tr>
+    </tfoot>`;       */
+    //Persistir em Local Storage
+};
+    
+    
+
 //Saldo final com destaque para lucro ou prejuizo
-//Persistir em Local Storage
+//atualizar a lista com extrato ja com o calculo atualizado
 
-//Todos os campos devem ser preenchidos
-//Mascara no campo valor (padrao 10,90)
-//Ao inserir transacao salvar em local storage e atualizar a lista com extrato ja com o calculo atualizado
-//Acao limpar dados (onclick) deve apresentar mensagem de confirmacao, apagar informacoes e atualizar a lista
+function calculateBalance(transactions){
+    
+    let balance = undefined;
+    for (var transaction in transactions){
+        balance += transactions[transaction].value;
+    }
+    return(balance);
 
-//Duvida: limpar dados deve limpar toda a tabela ou apenas o(s) item(s) selecionados?
+}
+
+function validateFields(e){
+    e.preventDefault();
+    //Todos os campos devem ser preenchidos
+    //Mascara no campo valor (padrao 10,90)
+}
+
+//Acao limpar dados (onclick) deve apresentar mensagem de confirmacao, apagar informacoes e atualizar a lista (todos)
+function cleanData(reg){
+    
+    if(confirm('Você está prestes a limpar todas as transações. Confirma?')){
+        delete reg;
+    }else{
+        alert('Operação cancelada. Os dados permanecem como estavam!')
+    };
+
+}
 
