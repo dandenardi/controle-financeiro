978ead8be19a7bfe1b8df6ceeae0286956778d03
Daniel do Amaral Denardi <dan.denardi@gmail.com>
2021-11-23 17:17:18 -0300

    acrescentada funcao de validacao e conversao para BRL

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ function handleStorage(){
     
 //incluir transacoes de compra ou venda de mercadoria
 function handleExchange(e){
-    //Ao inserir transacao salvar em local storage e 
+    //Ao inserir transacao salvar em local storage e (feito!)
     e.preventDefault();
     var transactions = handleStorage();
 
@@ -20,22 +20,36 @@ function handleExchange(e){
         name: e.target.elements['merch-name'].value,
         value: e.target.elements['merch-value'].value,
         kind: e.target.elements['transaction-type'].value,
-    })
+    });
     
-    console.log(e.target.elements['merch-name'].value);
-    console.log(e.target.elements['merch-value'].value);
-    console.log(e.target.elements['transaction-type'].value);
+    
 }
 
 
 //Criar um extrato das transacoes incluidas na ordem que foram inseridas
 function showTransactions() {
 
+    var transactions = handleStorage();
+
     console.log("carregado showTransactions");
+    console.log(transactions);
+    //apresenta as transacoes
+    document.querySelector('table .transactions thead').innerHTML =
+    //desenha só o head
+    `   <tr>
+            <td>
+                Mercadoria
+            </td>
+            <td>
+                Valor
+            </td>
+        </tr>`
 
-   /*  for (var transaction in transactions){
+    for (var transaction in transactions){
 
-        document.querySelector('table tbody').innerHTML +=
+    document.querySelector('table .transactions tbody').innerHTML +=
+    //desenha cada transacao
+    
         `<tr>
             <td>
                 ${transactions[transaction].name}
@@ -47,28 +61,29 @@ function showTransactions() {
                 ${transactions[transaction].kind}
             </td>
         </tr>`
-                   
+                        
     }
+    
+    document.querySelector('table .transactions tfoot').innerHTML =
+    //desenha o rodape que consiste no saldo
+    
     `<tfoot>
-                <tr>
-                    <td>
-                        Total
-                    </td>
-                    <td>
-                        ${calculateBalance()}
-                    </td>
-                </tr>
-    </tfoot>`;       */
-    //Persistir em Local Storage
+            <tr>
+                <td>
+                    Total
+                </td>
+                <td>
+                    ${calculateBalance()}
+                </td>
+            </tr>
+    </tfoot>`;
 };
-    
-    
 
 //Saldo final com destaque para lucro ou prejuizo
 //atualizar a lista com extrato ja com o calculo atualizado
 
 function calculateBalance(transactions){
-    
+    //funcao que calcula o saldo
     let balance = undefined;
     for (var transaction in transactions){
         balance += transactions[transaction].value;
@@ -78,12 +93,38 @@ function calculateBalance(transactions){
 }
 
 function validateFields(e){
+    
     e.preventDefault();
-    //Todos os campos devem ser preenchidos
-    //Mascara no campo valor (padrao 10,90)
+    var valuePattern = /[0-9]/g; 
+
+
+    if((valuePattern).test(e.key)){
+        //se o valor digitado for igual aos permitidos (numeros) a informacao fica, senao nem eh inserida
+        e.target.value += e.key;
+
+    
+    }
+    var validatedValue = e.target.value;
+    
+    return validatedValue;
 }
 
-//Acao limpar dados (onclick) deve apresentar mensagem de confirmacao, apagar informacoes e atualizar a lista (todos)
+function toBrCurrency(insertedValue){
+
+    var newValue = parseFloat(insertedValue);
+    var format = { minimumFractionDigits: 2, style: 'currency', currency: 'BRL'};
+    
+    var convertedValue = newValue.toLocaleString('pt-BR', format);
+    //merch-value estara com o valor convertido
+    
+    console.log(convertedValue);
+
+    return convertedValue;
+    
+}
+
+//Acao limpar dados (onclick) deve apresentar mensagem de confirmacao (feito),
+//apagar informacoes e atualizar a lista (todos)
 function cleanData(reg){
     
     if(confirm('Você está prestes a limpar todas as transações. Confirma?')){
